0cb5407abf97e697869a8520bae2929a80af8c64
Hugo <52917408+hugo-engineer@users.noreply.github.com>
2020-04-22 22:36:04 +1000

    webpack

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// https://dev.to/iamismile/how-to-setup-webpack-and-babel-for-react-59ph
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/styles.scss";
+
+const template = <p>Hello from react</p>;
+
+ReactDOM.render(template, document.getElementById("root"));
